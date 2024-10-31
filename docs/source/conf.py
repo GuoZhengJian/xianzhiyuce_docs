6ef2068fb91f9d7bb64d3ac9a63850c100da9f2f
GuoZhengJian <572078547@qq.com>
2024-10-31 15:31:50 +0800

    \

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ templates_path = ['_templates']
 
 # html_theme = 'sphinx_rtd_theme'
 # html_theme = 'nature'
-html_theme = 'press'
+# html_theme = 'press'
+html_theme = 'Material'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
